119e2ea1c1400a20004553c193cf68800ec63992
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-03 09:37:16 +0800

    Update the readme file.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData= Tag.findOne({
+    const tagData= await Tag.findOne({
       where: {
         id: req.params.id
       },
